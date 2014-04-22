cdb4d5bbe96602076a460619c33b067b24635ca8
skyndas <skyndas@97bdfe8d-690b-40ca-a606-c2e9c4a5822e>
2014-04-22 22:19:04 +0000

    Envi Template: Update script for download template over WebIf.

--- oscam-template/Envi_template/branches/script/UpdateEnviTemplate.sh
+++ oscam-template/Envi_template/branches/script/UpdateEnviTemplate.sh
@@ -4,16 +4,21 @@ cd /var/volatile/tmp
 wget -q http://www.streamboard.tv/svn/oscam-addons/oscam-template/Envi_template/branches/compressed_files/Envi_Template.tgz
 [ -f Envi_Template.tgz ] && mv -f Envi_Template.tgz /etc/tuxbox/config/tpl/Envi_Template.tgz
 cd /etc/tuxbox/config/tpl
-[ -f MENU.tpl ] && rm -f MENU.tpl
-[ -f HEADER.tpl ] && rm -f HEADER.tpl
-[ -f FOOTER.tpl ] && rm -f FOOTER.tpl
-[ -f HEADERSHORT.tpl ] && rm -f HEADERSHORT.tpl
-[ -f FILEMENU.tpl ] && rm -f FILEMENU.tpl
-[ -f FILEEDITCSS.tpl ] && rm -f FILEEDITCSS.tpl
+[ -f CACHEEXPAGE.tpl ] && rm -f CACHEEXPAGE.tpl
 [ -f CLIENTHEADLINEBIT.tpl ] && rm -f CLIENTHEADLINEBIT.tpl
 [ -f CLIENTHEADLINEWITH.tpl ] && rm -f CLIENTHEADLINEWITH.tpl
 [ -f CLIENTPHEADLINE.tpl ] && rm -f CLIENTPHEADLINE.tpl
 [ -f CLIENTRHEADLINE.tpl ] && rm -f CLIENTRHEADLINE.tpl
+[ -f CONFIGMENU.tpl ] && rm -f CONFIGMENU.tpl
+[ -f FAILBAN.tpl ] && rm -f FAILBAN.tpl
+[ -f FILEEDITCSS.tpl ] && rm -f FILEEDITCSS.tpl
+[ -f FILEMENU.tpl ] && rm -f FILEMENU.tpl
+[ -f FOOTER.tpl ] && rm -f FOOTER.tpl
+[ -f HEADERSHORT.tpl ] && rm -f HEADERSHORT.tpl
+[ -f MENU.tpl ] && rm -f MENU.tpl
+[ -f READERS.tpl ] && rm -f READERS.tpl
+[ -f STATUS.tpl ] && rm -f STATUS.tpl
+[ -f USERCONFIGLIST.tpl ] && rm -f USERCONFIGLIST.tpl
 tar -xzvf Envi_Template.tgz
 mv -f Envi_Template.css /etc/tuxbox/config/Envi_Template.css
 rm -if Envi_Template.tgz || exit 1
